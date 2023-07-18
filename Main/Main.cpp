50d55a6c0f0fcc85a8ec8c055799377ffc24bc99
HuyAndCode <121392156+QHuyyyyyyyyyyy@users.noreply.github.com>
2023-07-18 16:49:53 +0700

    Add files via upload

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -2,7 +2,39 @@
 
 int main(int argc, char** argv)
 {
-	cout << "HCMUS HCMUS";
+	if (argc < 2)	return 0;
+
+	int require = check_command(argc, argv);
+	switch (require)
+	{
+		case 1:
+		{
+			command1(argv);
+			break;
+		}
+		case 2:
+		{
+			command2(argv);
+			break;
+		}
+		case 3:
+		{
+			/*command3(argv);*/
+			break;
+		}
+		case 4:
+		{
+			/*command4(argv);*/
+			break;
+		}
+		case 5:
+		{
+			/*command5(argv);*/
+			break;
+		}
+	default:
+		break;
+	}
 
 	return 0;
 }
