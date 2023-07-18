d860f8e0ac2e6e4bf86ca5aaaf0eb46f5cb54c09
TonyTran2902 <128487684+TonyTran2902@users.noreply.github.com>
2023-07-18 23:01:08 +0700

    Update Source.cpp

--- Main/Source.cpp
+++ Main/Source.cpp
@@ -102,3 +102,117 @@ void command2(char** argv)
 		cout << "Comparisons: " << endl;
 	}
 }
+
+void command3(char** argv)
+{
+/*  – Prototype: [Execution file] -a [Algorithm] [Input size] [Output parameter(s)]
+	– Ex: a.exe -a binary-insertion-sort 70000 -comp  */
+	string alg = handle_algorithm_name(argv[2]);
+	int inputSize = stoi(argv[3]);
+	string requireOutput = argv[4];
+	
+	//Sort
+
+	// ===== print in the format =====
+	cout << "ALGORITHM MODE\n";
+	cout << "Algorithm: " << alg << endl;
+	cout << "Input size: " << inputSize << endl << endl;
+	cout << "Input order: " << "Randomize" << endl;
+	cout << "-------------------\n";
+	if (requireOutput == "-time") {
+		cout << "Running time: " << endl << endl;
+
+		cout << "Input order: " << "Nearly Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl << endl;
+
+		cout << "Input order: " << "Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl << endl;
+
+		cout << "Input order: " << "Reserved" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl << endl;
+	}
+	else if (requireOutput == "-comp") {
+		cout << "Comparisons: " << endl << endl;
+
+		cout << "Input order: " << "Nearly Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Comparisons: " << endl << endl;
+		
+		cout << "Input order: " << "Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Comparisons: " << endl << endl;
+		
+		cout << "Input order: " << "Reserved" << endl;
+		cout << "-------------------\n";
+		cout << "Comparisons: " << endl << endl;
+	}
+	else if (requireOutput == "-both") {
+		cout << "Running time: " << endl;
+		cout << "Comparisons: " << endl << endl;
+
+		cout << "Input order: " << "Nearly Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl;
+		cout << "Comparisons: " << endl << endl;
+
+		cout << "Input order: " << "Sorted" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl;
+		cout << "Comparisons: " << endl << endl;
+
+		cout << "Input order: " << "Reserved" << endl;
+		cout << "-------------------\n";
+		cout << "Running time: " << endl;
+		cout << "Comparisons: " << endl << endl;
+	}
+
+}
+
+void command4(char** argv)
+{
+	//– Prototype: [Execution file] -c [Algorithm 1] [Algorithm 2] [Given input]
+	//– Ex : a.exe - c heap - sort merge - sort input.txt
+	string alg1 = handle_algorithm_name(argv[2]);
+	string alg2 = handle_algorithm_name(argv[3]);
+	string fileName = argv[4];
+
+	int inputSize;
+	fstream fp;
+	fp.open(fileName, ios::in);
+	fp >> inputSize;
+	fp.close();
+
+	//Sort
+
+	// ===== print in the format =====
+	cout << "ALGORITHM MODE\n";
+	cout << "Algorithm: " << alg1 << " | " << alg2 << endl;
+	cout << "Input file: " << fileName << endl;
+	cout << "Input size: " << inputSize << endl;
+	cout << "-------------------\n";
+	cout << "Running time: " << endl;
+	cout << "Comparisons: " << endl << endl;
+}
+
+void command5(char** argv)
+{
+	//[Execution file] -c [Algorithm 1] [Algorithm 2] [Input size] [Input order]
+	string alg1 = handle_algorithm_name(argv[2]);
+	string alg2 = handle_algorithm_name(argv[3]);
+	int inputSize = stoi(argv[4]);
+	string inputOrder = handle_input_order(argv[5]);
+
+	//Sort
+
+	// ===== print in the format =====
+	cout << "ALGORITHM MODE\n";
+	cout << "Algorithm: " << alg1 << " | " << alg2 << endl;
+	cout << "Input size: " << inputSize << endl;
+	cout << "Input order: " << inputOrder << endl;
+	cout << "-------------------\n";
+	cout << "Running time: " << endl;
+	cout << "Comparisons: " << endl << endl;
+}
